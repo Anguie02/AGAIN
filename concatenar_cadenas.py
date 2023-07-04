207689b1aa78406c9af6ea91a14bfbf71e4c2d6f
Anguie02 <1487186@senati.pe>
2023-07-03 20:44:14 -0500

    Proyecto1

---
+++ concatenar_cadenas.py
@@ -0,0 +1,17 @@
+#Concatenar cadenas
+# print("Aprende a programar con "+ variable)
+# print("Aprende a programar con {}".format(variable))
+# print(f"Aprende a programar con {variable}") #f-string
+
+
+# Historias locas (Mad libs)
+#Definimos las variables que completaran el parráfo
+adj = input("Adjetivo: ")
+verbo1 = input("Verbo: ")
+verbo2 = input("Verbo: ")
+sustantivo_plural = input("Sustantivo(plural): ")
+# Definimos la variable parráfo 
+madlib = f"Programas es tan {adj}. Siempre me emociona porque me encanta{verbo1} problemas. ¡Aprende a {verbo2} con Imaginación a la realidad y alcanza tus {sustantivo_plural}!"
+#Mostramos
+print(madlib)
+
